863c3836df4854ef80d7a05a2dc72757f4c8fcfe
GET Academy <terje@kolderup.net>
2026-09-02 09:06:15 +0200

    Initial commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SimpleShop;Integrated Security=True";
+var sql = """
+              SELECT
+              o.Id AS OrderId,
+              o.CreatedUtc,
+              o.TotalAmount,
+
+              c.Id AS CustomerId,
+              c.Name AS CustomerName,
+              c.Email AS CustomerEmail
+
+          FROM Orders o
+
+          JOIN Customers c
+              ON c.Id = o.CustomerId;
+          """;
+
